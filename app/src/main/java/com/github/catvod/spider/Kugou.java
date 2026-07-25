9388c0cb51611d5a0a8af822b4624bad1a51db2a
qiang <163614031+jiutv@users.noreply.github.com>
2026-07-26 05:18:06 +0800

    Update Kugou.java

--- app/src/main/java/com/github/catvod/spider/Kugou.java
+++ app/src/main/java/com/github/catvod/spider/Kugou.java
@@ -16,15 +16,13 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * @author Qile
- */
 public class Kugou extends Spider {
 
     private Map<String, String> getHeader() {
@@ -39,7 +37,9 @@ public class Kugou extends Spider {
         List<Vod> list = new ArrayList<>();
         List<String> typeIds = Arrays.asList("6666|0", "33162|1", "4681|2");
         List<String> typeNames = Arrays.asList("热门榜单", "特色音乐榜", "全球榜");
-        for (int i = 0; i < typeIds.size(); i++) classes.add(new Class(typeIds.get(i), typeNames.get(i)));
+        for (int i = 0; i < typeIds.size(); i++) {
+            classes.add(new Class(typeIds.get(i), typeNames.get(i)));
+        }
         return Result.string(classes, list);
     }
 
@@ -57,41 +57,64 @@ public class Kugou extends Spider {
         Elements lis = doc.select(".pc_rank_sidebar").eq(digitValue).select("ul li a");
         JSONArray videos = new JSONArray();
         for (Element li : lis) {
-            String vid = li.attr("href");
-            String name = li.attr("title");
-            JSONObject vod = new JSONObject().put("vod_id", vid).put("vod_name", name);
+            String rankUrl = li.attr("href");
+            String rankTitle = li.attr("title");
+            JSONObject vod = new JSONObject()
+                    .put("vod_id", rankUrl)
+                    .put("vod_name", rankTitle);
             videos.put(vod);
         }
-        JSONObject result = new JSONObject().put("total", lis.size()).put("pagecount", 1).put("list", videos);
+        JSONObject result = new JSONObject()
+                .put("total", lis.size())
+                .put("pagecount", 1)
+                .put("list", videos);
         return result.toString();
     }
 
     @Override
     public String detailContent(List<String> ids) throws Exception {
-        Document doc = Jsoup.parse(OkHttp.string(ids.get(0), getHeader()));
+        String rankUrl = ids.get(0);
+        Document doc = Jsoup.parse(OkHttp.string(rankUrl, getHeader()));
         Elements playlist = doc.select(".pc_temp_songlist ul li");
         List<String> vodItems = new ArrayList<>();
-        for (int j = 0; j < playlist.size(); j++) {
-            Element a = playlist.get(j);
-            String href = a.select("a.pc_temp_songname").attr("href");
-            String text = a.select("a.pc_temp_songname").text();
-            vodItems.add(text + "$" + href);
+        for (Element item : playlist) {
+            String songName = item.select("a.pc_temp_songname").text().trim();
+            if (!TextUtils.isEmpty(songName)) {
+                // 选集格式：歌名$歌名，播放时把名称传给player
+                vodItems.add(songName + "$" + songName);
+            }
         }
-        String title = doc.select(".pc_temp_title h3").text();
-        String remark = doc.select(".rank_update").text();
-        String vod_play_from = "Qile";
-        String vod_play_url = TextUtils.join("#", vodItems);
+        String title = doc.select(".pc_temp_title h3").text().trim();
+        String remark = doc.select(".rank_update").text().trim();
+
         Vod vod = new Vod();
-        vod.setVodId(ids.get(0));
+        vod.setVodId(rankUrl);
         vod.setVodName(title);
         vod.setVodRemarks(remark);
-        vod.setVodPlayFrom(vod_play_from);
-        vod.setVodPlayUrl(vod_play_url);
+        vod.setVodPlayFrom("酷狗榜单");
+        vod.setVodPlayUrl(TextUtils.join("#", vodItems));
         return Result.string(vod);
     }
 
     @Override
-    public String playerContent(String flag, String id, List<String> vipFlags) throws Exception {
-        return Result.get().url(id).parse().header(getHeader()).string();
+    public String playerContent(String flag, String songName, List<String> vipFlags) throws Exception {
+        if (TextUtils.isEmpty(songName)) throw new Exception("歌曲名称为空");
+        String keyword = URLEncoder.encode(songName, "UTF-8");
+        // meting api 酷狗音源搜索
+        String searchApi = String.format("https://api.injahow.cn/meting/api?server=kg&type=search&keywords=%s", keyword);
+        String response = OkHttp.string(searchApi, getHeader());
+        JSONObject json = new JSONObject(response);
+        JSONArray data = json.getJSONArray("data");
+        if (data.length() == 0) throw new Exception("未找到音源");
+
+        JSONObject track = data.getJSONObject(0);
+        String audioUrl = track.optString("url");
+        if (TextUtils.isEmpty(audioUrl)) throw new Exception("暂无试听资源");
+
+        // 返回真实音频直链，TVBox播放器直接解码播放
+        return Result.get()
+                .url(audioUrl)
+                .header(getHeader())
+                .string();
     }
 }
