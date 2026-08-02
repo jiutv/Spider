89df8e7ca6e7e5cc8796e09a1ad23298942a9feb
qiang <163614031+jiutv@users.noreply.github.com>
2026-08-03 01:11:30 +0800

    Update KuKuMusic.java

--- app/src/main/java/com/github/catvod/spider/KuKuMusic.java
+++ app/src/main/java/com/github/catvod/spider/KuKuMusic.java
@@ -14,6 +14,7 @@ import org.json.JSONObject;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
 
